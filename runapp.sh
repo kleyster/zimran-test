251d1946632abc7b482a13b4fea2ed83b94b9228
kleyster <aubakirof@gmail.com>
2022-08-04 21:49:08 +0600

    update: script to launch web app

--- runapp.sh
+++ runapp.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
-mkdir ./data/logs/
-
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
 
-exec gunicorn core.wsgi:application -b 0.0.0.0:8000 --reload --log-level='debug' --log-file=./data/logs/gunicorn.log --access-logfile=./data/logs/gunicorn-access.log --error-logfile=./data/logs/gunicorn-error.log
+exec gunicorn core.wsgi:application -b 0.0.0.0:8000 --reload --log-level=INFO
